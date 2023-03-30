e8a3dbbe0f63734d168fa367b1087ec3e0157932
evensty <maxim.veselov11@gmail.com>
2023-03-30 23:05:25 +0300

    add allure steps

--- demoqa/model/pages/practice_form.py
+++ demoqa/model/pages/practice_form.py
@@ -1,5 +1,9 @@
 from selene import have
 from selene.support.shared import browser
+import allure
+from allure import step
+from allure_commons.types import Severity
+
 
 from demoqa.model.controls.datepicker import Datepicker
 from demoqa.model.controls.checkbox import CheckBox
@@ -56,31 +60,48 @@ class PracticePage:
         browser.element('.form-file').submit()
 
     def fill_form(self):
-        browser.open('https://demoqa.com/automation-practice-form')
-        self.set_fullname()
-        self.set_email()
-        self.select_gender()
-        self.set_mobile_number()
-        self.set_birthday()
-        self.set_subjects()
-        self.select_hobbies()
-        self.select_picture()
-        self.set_current_address()
-        self.select_state()
-        self.select_city()
-        self.submit()
+        allure.dynamic.tag("web")
+        allure.dynamic.severity(Severity.BLOCKER)
+        allure.dynamic.story("Проверка формы регистрации")
+        with step("Переходим на страницу заполнения формы"):
+            browser.open('https://demoqa.com/automation-practice-form')
+        with step("Вводим имя"):
+            self.set_fullname()
+        with step("Вводим почту"):
+            self.set_email()
+        with step("Указываем пол"):
+            self.select_gender()
+        with step("Вводим номер телефона"):
+            self.set_mobile_number()
+        with step("Указываем дату рождения"):
+            self.set_birthday()
+        with step("Указываем предметы"):
+            self.set_subjects()
+        with step("Выбираем хобби"):
+            self.select_hobbies()
+        with step('Прикладываем картинку'):
+            self.select_picture()
+        with step("Указываем адрес"):
+            self.set_current_address()
+        with step("Выбираем штат"):
+            self.select_state()
+        with step("Выбираем город"):
+            self.select_city()
+        with step("Подтверждаем регистрацию"):
+            self.submit()
 
     def check_results(self):
-        browser.element('.table').all('td').even.should(have.texts(
-            f'{self.user.first_name} {self.user.last_name}',
-            self.user.email,
-            self.user.gender,
-            self.user.phone,
-            f'{self.user.day} May,{self.user.year}',
-            self.user.subject,
-            self.user.hobbies,
-            self.user.image,
-            self.user.address,
-            f'{self.user.state} {self.user.city}'))
+        with step("Проверяем форму на корректное заполнение"):
+            browser.element('.table').all('td').even.should(have.texts(
+                f'{self.user.first_name} {self.user.last_name}',
+                self.user.email,
+                self.user.gender,
+                self.user.phone,
+                f'{self.user.day} May,{self.user.year}',
+                self.user.subject,
+                self.user.hobbies,
+                self.user.image,
+                self.user.address,
+                f'{self.user.state} {self.user.city}'))
 
 
